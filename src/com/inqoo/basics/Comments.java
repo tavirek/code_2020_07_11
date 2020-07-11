ce0e36fe91257a9c3d05a75fcee69ffd43e1d1e6
tavirek <przemyslawdopart@gmail.com>
2020-07-11 12:36:55 +0200

    Dodano komentarz, usunieto system out

--- src/com/inqoo/basics/Comments.java
+++ src/com/inqoo/basics/Comments.java
@@ -5,6 +5,7 @@ public class Comments {
     public static void main(String[] args) {
         System.out.println("Witaj na "); //Powitanie
         System.out.println("kursie programowania");
+// jakis komentarz
 
         /* Na koniec emotka,
           bo powaga zabija ;)
