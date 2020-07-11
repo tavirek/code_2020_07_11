03a18cd38c48e90be97017b1e2f3953f282eb8a8
tavirek <przemyslawdopart@gmail.com>
2020-07-11 12:46:08 +0200

    zmieniono powitanie znowu

--- src/com/inqoo/basics/HelloWorld.java
+++ src/com/inqoo/basics/HelloWorld.java
@@ -3,6 +3,6 @@ package com.inqoo.basics;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Cześć!");
+        System.out.println("Hej z brancha cokolwiek!");
     }
 }
